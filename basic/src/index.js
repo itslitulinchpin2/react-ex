a8e536270826939b049ce72278e902ed61d1e35e
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-03-29 14:57:44 +0100

    creating form

--- basic/src/index.js
+++ basic/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
+import AppForm from './AppForm';
 
-import AppMentors from './AppMentors';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentors/>
+    <AppForm/>
   </React.StrictMode>
 );
 
