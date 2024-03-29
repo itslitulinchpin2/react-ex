a591e821708c4611313db649efa024569a7a7868
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-03-29 14:28:34 +0100

    add useReducer logic

--- basic/src/AppMentors.jsx
+++ basic/src/AppMentors.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useReducer } from 'react'
+import personReducer from './reducer/person-reducer'
 export default function AppMentor() {
     const initialPerson={  
     name:'lee',
@@ -14,15 +14,24 @@ export default function AppMentor() {
         title:"writer"
         }]
     }
-    const[person,setPerson]=useState(initialPerson)
+    //const[person,setPerson]=useState(initialPerson)
+    const [person,dispatch]=useReducer(personReducer,initialPerson);
+    const handleUpdate=()=>{
+        const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
+        const current = prompt('이름을 무엇으로 바꾸시겠어요?');
+        dispatch({type:'updated',prev,current})
+    }
 
-    // const findIndex = (input)=>{
-    //     for(let i=0;i<person.mentors.length;i++){
-    //         if(person.mentors[i].name===input){
-    //             return i;
-    //         }
-    //     }
-    // }
+    const addMentor = ()=>{
+        const name = prompt('추가할 멘토의 이름은?');
+        const title=prompt('추가할 사람의 타이틀은?');
+       dispatch({type:'added',name,title})
+    }
+
+    const deleteMentor = ()=>{
+        const name=prompt('삭제할 사람의 이름은?');
+       dispatch({type:'deleted',name})
+    }
     
   return (
     <div>
@@ -41,31 +50,9 @@ export default function AppMentor() {
             )}
         </ul>
 
-        <button
-        onClick={()=>{
-            const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
-            // let foundIndex = findIndex(prev);
-            // console.log(foundIndex);
-            const current = prompt('이름을 무엇으로 바꾸시겠어요?');
-            setPerson({...person, mentors:person.mentors.map((mentor)=>{
-                if(mentor.name===prev){
-                    return {...mentor, name:current}
-                }  return mentor;})
-            })
-        }}>멘토 이름 바꾸기</button>
-        <button onClick = {()=>{
-            const name = prompt('추가할 멘토의 이름은?');
-            const title=prompt('추가할 사람의 타이틀은?');
-            person.mentors.push({name:name,title:title});
-            setPerson({...person,mentors:[...person.mentors]})
-        }}>멘토 추가하기</button>
-        <button onClick={()=>{
-            const name=prompt('삭제할 사람의 이름은?');
-            setPerson({...person, mentors:person.mentors.filter((mentor)=>mentor.name!==name)
-            })
-        }}>멘토 삭제하기</button>
-
-        
+        <button onClick={handleUpdate}>멘토 이름 바꾸기</button>
+        <button onClick = {addMentor}>멘토 추가하기</button>
+        <button onClick={deleteMentor}>멘토 삭제하기</button>  
       
     </div>
   )
