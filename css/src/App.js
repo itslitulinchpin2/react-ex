c76a88865a47f5fc4a86768a95a07e683bdd179d
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-04-01 08:42:53 +0200

    styled-components

--- css/src/App.js
+++ css/src/App.js
@@ -2,12 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import Button1 from './Button1';
 import Button2 from './Button2';
+import styled, {css} from 'styled-components';
+
+const Container = styled.div`
+  display:flex
+
+`
+
+const Button = styled.button`
+  background:transparent;
+  border-radius:3px;
+  border: 2px solid green;
+  color:black;
+  margin: 0 1em;
+  padding: 0.2em 1em;
+  ${(props) => props.primary &&
+  css `
+  background:pink;
+  color:white;`};
+
+`;
 
 function App() {
   return (
    <>
    <Button1/>
    <Button2/>
+   <Container>
+    <Button primary >Button</Button>
+    <Button>Button</Button>
+   </Container>
+   
    </>
   );
 }
