620e9d3b7a85d84c3a8b2a6ae03c0fa6ccb662be
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-04-03 21:16:22 +0200

    lists CRUD

--- wish-list/src/App.js
+++ wish-list/src/App.js
@@ -4,23 +4,54 @@ import { useState } from 'react';
 import List from './components/List';
 function App() {
 
+  const [todos,setTodos]=useState([
+    {content:'hahaha',done:false},
+    {content:'hahahahaha',done:true}
+  ]);
+
+  const [inputValue,setInputValue]=useState('');
+
+  function modeChange(){
+    console.log('change color');
+  }
+  function showAll(){
+    console.log('showAll');
+  }
+  function showActive(){
+    console.log('show active');
+  }
+  function showCompleted(){
+    console.log('show completed');
+  }
+  function handleInputChange(e){
+    //console.log(e.target.value);
+    setInputValue(e.target.value);
+  }
+  function addTodo(){
+    //console.log(inputValue);
+    todos.push({content:inputValue, done:false})
+    setTodos([...todos]);
+    setInputValue('')
+  }
 
   return (
   <div className="page">
   <nav className="nav">
-    <button id="icon">다크모드</button>
-    <button>All</button>
-    <button>Active</button>
-    <button>Completed</button>
+    <button onClick={modeChange} id="icon">다크모드</button>
+    <button onClick={showAll}>All</button>
+    <button onClick={showActive}>Active</button>
+    <button onClick={showCompleted}>Completed</button>
   </nav>
   <section className="section">
-    <List content="haha"></List>
-    <List content="hahaha"></List>
-    <List content="hahahaha"></List>
+    {
+    todos.map((todo)=>{
+    return <List key= {todo.content} content={todo.content} done={todo.done} todos={todos} setTodos={setTodos}/>
+    })
+    }
   </section>
   <footer className="footer">
-    <textarea className="textarea" placeholder='add todo'></textarea>
-    <button className="add">add</button>
+    <textarea  value={inputValue} onChange={handleInputChange} className="textarea" placeholder='add todo'></textarea>
+    <button onClick={addTodo} className="add">add</button>
   </footer>
   </div>
   );
--- wish-list/src/components/List.jsx
+++ wish-list/src/components/List.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import styles from './List.module.css';
-export default function List({content}) {
+export default function List({content,done,todos,setTodos}) {
+
+    function deleteList(content){
+        console.log('delete it!')
+        setTodos(todos.filter(todo=>todo.content!==content))
+    }
+    const handleCheckboxChange=()=>{
+        setTodos(todos.map(todo=>{
+            if(todo.content===content){
+                return {...todo, done:!done};
+            }
+            return todo;
+        }))
+    }
+ 
   return (
     <div className={styles.list} id="list">
-        <checkbox></checkbox>
-      {content}
-      <button className={styles.delete}>삭제하기</button>
+      <input type="checkbox"
+            checked={done}
+            onChange={handleCheckboxChange}/>
+        {content}
+      <button onClick={()=>deleteList(content)} className={styles.delete}>삭제하기</button>
+     
     </div>
   )
 }
